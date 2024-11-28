d07f600022fae1645564b95782f734384b13bb44
alpha <amitk.vishwa1633@gmail.com>
2024-11-28 22:29:22 +0530

    Done some modifications

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+
+const ExploreMenu = ({category,setCategory}) => {
+  return (
+    <div className="explore-menu flex flex-col gap-5" id="explore-menu">
+      <h1 className="text-[#262626] font-bold">Explore our menu</h1>
+      <p className="explore-menu-text">Choose from a diverse menu featuring a delecate array of dishes. Our mission is to satisfy your carving and elevate your dinning experience, one delicious meal at a time.</p>
+    <div className="explore-menu-list flex justify-between items-center gap-7.5 text-center mx-5 my-0 overflow-x-scroll">
+        {menu_list.map((item,index)=>{
+            return(
+                <div onClick={()=> setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                    <img className={category===item.menu_name? "active" : ""} src={item.menu_image} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        }   )}
+    </div>
+    <hr/>
+    </div>
+  );
+};
+
+export default ExploreMenu;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,9 +4,10 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-    <div className='header-contents'>
-    <h2>Order your favourite food here</h2>
-    <p>Choose from a diverse menu featuring a delecated array of dishes crafted with the finest ingridients and culinary expertise. Our mission is to satisfy your carvings and elevate your dining experience, one delicious meal at a time.</p>
+    <div className='header-contents absolute flex flex-col items-start'>
+    <h2 className='font-medium text-white'>Order your favourite food here</h2>
+    <p className='text-white text-[1vw]'>Choose from a diverse menu featuring a delecated array of dishes crafted with the finest ingridients and culinary expertise. Our mission is to satisfy your carvings and elevate your dining experience, one delicious meal at a time.</p>
+    <button className='h2 border-0 text-[#747474] font-medium px-[1vw] py-2 bg-white text-sm rounded-[50px]'>View Menu</button>
     </div>       
     </div>
   )
