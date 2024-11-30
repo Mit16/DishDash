8ccadfdbfb2da97f9793a636ba02663cd5b2ebef
alpha <amitk.vishwa1633@gmail.com>
2024-11-30 09:59:33 +0530

    Modified cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -29,7 +29,9 @@ const Cart = () => {
                   <p>₹ {item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>₹ {item.price * cartItems[item._id]}</p>
-                  <p onClick={()=>removeFromCart(item._id)} className="cross">❌</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    ❌
+                  </p>
                 </div>
                 <hr />
               </>
@@ -38,7 +40,35 @@ const Cart = () => {
         })}
       </div>
       <div className="cart-bottom">
-
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have promo code,Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="Enter a Promocode" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
