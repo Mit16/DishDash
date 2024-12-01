9c583acae20271f208522def3569f881516ca0e3
alpha <amitk.vishwa1633@gmail.com>
2024-12-01 07:44:23 +0530

    Stored the key in env and imported it

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DB = process.env.MONGODB;
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://amitkvishwa1633:Y94bUsaU0GIhLURb@food-zzz.yue0y.mongodb.net/Food-Zzz').then(()=>console.log("DB Connected"));
-}
+    try {
+        await mongoose.connect(DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("DB Connected");
+    } catch (error) {
+        console.error("Error connecting to DB:", error.message);
+        process.exit(1);
+    }
+};
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// const DB = process.env.MONGODB;
+
+
+// export const connectDB = async () => {
+//     await mongoose.connect(DB).then(()=>console.log("DB Connected"));
+// }
